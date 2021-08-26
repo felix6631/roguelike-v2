498b457fb1638ee36e7884a921b55c3dd2bfd1fa
felix6631 <83746046+felix6631@users.noreply.github.com>
2021-08-26 20:44:58 +0900

    weapon.cs
    aiming code added (v0.1, error included)

--- move the word/Assets/Script/weapon.cs
+++ move the word/Assets/Script/weapon.cs
@@ -7,20 +7,25 @@ public class weapon : MonoBehaviour
     public GameObject bullet;
     public Transform sPoint;//마우스 포인터
     public float timeBetweenshots;//연사 속도
+    float angle;
+    Vector2 target, mouse;
 
     private float shotTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         //Vector2 diretion = Camera.main.ScreenToWorldPoint(Input.mousePosition)
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
 
         }
