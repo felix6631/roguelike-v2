253f85674623699606e379c556c32e2bf5f0d889
felix6631 <83746046+felix6631@users.noreply.github.com>
2021-09-21 21:45:18 +0900

    Lobby Materiallization
    r e t r 0  i s  G O D

---
+++ move the word/Assets/Script/LobbyManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+
+    public Text connectionInfoText;
+    public Button joinButton;
+    
+    // try to connect to master server
+    void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        joinButton.interactable = false;
+        connectionInfoText.text = "Connecting...";
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        joinButton.interactable = true;
+        connectionInfoText.text = "Online";
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        //base.OnDisconnected(cause);
+        joinButton.interactable = false;
+        connectionInfoText.text = "Offline";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Connect()
+    {
+        Debug.Log("foo");
+        joinButton.interactable = false;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "Connecting...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectionInfoText.text = "Offline";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        //base.OnJoinRandomFailed(returnCode, message);
+        connectionInfoText.text = "Creating Empty...";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //base.OnJoinedRoom();
+        connectionInfoText.text = "Connected Successfully!";
+        PhotonNetwork.LoadLevel("Main");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (joinButton.IsActive())
+        {
+            this.Connect();
+            return;
+        }
+        */
+
+    }
+}
